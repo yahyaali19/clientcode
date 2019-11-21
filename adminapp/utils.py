504ef8679b95f1329c5e54bbb464a5130c53984b
practice <l144179@lhr.nu.edu.pk>
2019-11-21 23:44:54 +0500

    Aggregate sum is dict

--- adminapp/utils.py
+++ adminapp/utils.py
@@ -6,4 +6,4 @@ from django.db.models import Sum
 def calculate_progress_bar():
     target_total = models.Representative.objects.aggregate(Sum('target'))
     monthly_FTD_total = models.Representative.objects.aggregate(Sum('monthly_FTD'))
-    return monthly_FTD_total/target_total * 100
+    return monthly_FTD_total['monthly_FTD__sum']/target_total['target__sum'] * 100
