d7038d1bfd033fd378ca29e5f1fbe31c56b4fded
practice <l144179@lhr.nu.edu.pk>
2019-11-21 19:24:25 +0000

    Rearrange table

--- adminapp/utils.py
+++ adminapp/utils.py
@@ -6,12 +6,13 @@ from django.db.models import Sum
 def calculate_progress_bar():
     target_total = models.Representative.objects.aggregate(Sum('target'))
     monthly_FTD_total = models.Representative.objects.aggregate(Sum('monthly_FTD'))
-    return monthly_FTD_total['monthly_FTD__sum']/target_total['target__sum'] * 100
+    percentage = monthly_FTD_total['monthly_FTD__sum']/target_total['target__sum'] * 100
+    return round(percentage, 2)
 
 def all_sum():
-    target_total = models.Representative.objects.aggregate(Sum('target'))
-    monthly_FTD_total = models.Representative.objects.aggregate(Sum('monthly_FTD'))
-    monthly_amount_total = models.Representative.objects.aggregate(Sum('monthly_amount'))
-    daily_FTD_total = models.Representative.objects.aggregate(Sum('daily_FTD'))
-    daily_amount_total = models.Representative.objects.aggregate(Sum('daily_amount'))
+    target_total = models.Representative.objects.aggregate(Sum('target'))['target__sum']
+    monthly_FTD_total = models.Representative.objects.aggregate(Sum('monthly_FTD'))['monthly_FTD__sum']
+    monthly_amount_total = models.Representative.objects.aggregate(Sum('monthly_amount'))['monthly_amount__sum']
+    daily_FTD_total = models.Representative.objects.aggregate(Sum('daily_FTD'))['daily_FTD__sum']
+    daily_amount_total = models.Representative.objects.aggregate(Sum('daily_amount'))['daily_amount__sum']
     return target_total, monthly_FTD_total, monthly_amount_total, daily_FTD_total, daily_amount_total
